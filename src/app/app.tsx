92707ef39b4ca2547ef99c787e3d3b59409d4947
Bùi Yến Nhi <buidoanhaiyen@gmail.com>
2025-05-24 15:35:12 +0700

    Update Dự Án, Thông tin cá nhân

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -6,11 +6,6 @@ import { AuthProvider } from '@cnpm/context/AuthContext';
 import SignInPage from '@cnpm/pages/LoginPage';
 import SignUpPage from '@cnpm/pages/SignUp';
 import LoginError from '@cnpm/pages/LoginError';
-import DashboardHoiOngThamInh from '@cnpm/pages/DashboardHoiOngThamInh';
-import DashboardQuanTriVien from '@cnpm/pages/DashboardQuanTriVien';
-import DashboardQuanTriVien1 from '@cnpm/pages/DashboardQuanTriVien1';
-import DashboardQuanTriVien2 from '@cnpm/pages/DashboardQuanTriVien2';
-import AdminDashboard from '@cnpm/pages/AdminDashboard';
 import { PublicRoute } from '@cnpm/components/Protect/PublicRoute';
 import DuAn from '@cnpm/pages/DuAn';
 import Profile from '@cnpm/pages/Profile';
@@ -32,11 +27,6 @@ export function App() {
               <Route path="/signin" element={<SignInPage />} />
               <Route path="/signup" element={<SignUpPage />} />
               <Route path="/login-error" element={<LoginError />} />
-              <Route path="/dashboard" element={<DashboardHoiOngThamInh />} />
-              <Route path="/quantrivien" element={<DashboardQuanTriVien />} />
-              <Route path="/quantrivien1" element={<DashboardQuanTriVien1 />} />
-              <Route path="/quantrivien2" element={<DashboardQuanTriVien2 />} />
-              <Route path="/admin" element={<AdminDashboard />} />
               <Route path="/duan" element={<DuAn />} />
               <Route path="/profile" element={<Profile />} />
             </Route>
