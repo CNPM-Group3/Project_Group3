41b12df2b8f392588c5d91a3a0ed00dd84f67971
Nguyen Minh Nguyet <nguyetnm4741@ut.edu.vn>
2025-06-01 22:10:51 +0700

    update

--- obj/Debug/net8.0/SRPM.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/SRPM.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SRPM.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dfc7584d6c1b182b34a7e97d7cd077750ae213e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd15a9a817704f09446b46a73e14c19f1806a153")]
 [assembly: System.Reflection.AssemblyProductAttribute("SRPM.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SRPM.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
