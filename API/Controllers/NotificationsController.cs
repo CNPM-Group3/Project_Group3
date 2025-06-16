7112a88c83666cd16207ce1663b88c4778e4961e
Hong Dao <daon53220@gmail.com>
2025-06-16 19:29:06 +0700

    Update NotificationController

--- API/Controllers/NotificationsController.cs
+++ API/Controllers/NotificationsController.cs
@@ -4,6 +4,7 @@ using SRPM.API.Models;
 using SRPM.API.Services;
 using System.Security.Claims;
 using Task = System.Threading.Tasks.Task;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace SRPM.API.Controllers
 {
@@ -20,6 +21,7 @@ namespace SRPM.API.Controllers
         }
 
         [HttpGet("{id}")]
+        [SwaggerOperation(Summary = "Lấy thông báo theo ID")]
         public async Task<ActionResult<NotificationDto>> GetById(int id)
         {
             var userId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
@@ -31,6 +33,7 @@ namespace SRPM.API.Controllers
         }
 
         [HttpGet]
+        [SwaggerOperation(Summary = "Lấy tất cả thông báo của người dùng")]
         public async Task<ActionResult<IEnumerable<NotificationDto>>> GetByUserId()
         {
             var userId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
@@ -39,6 +42,7 @@ namespace SRPM.API.Controllers
         }
 
         [HttpGet("unread")]
+        [SwaggerOperation(Summary = "Lấy thông báo chưa đọc")]
         public async Task<ActionResult<IEnumerable<NotificationDto>>> GetUnreadByUserId()
         {
             var userId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
@@ -47,6 +51,7 @@ namespace SRPM.API.Controllers
         }
 
         [HttpPatch("{id}/mark-as-read")]
+        [SwaggerOperation(Summary = "Đánh dấu thông báo là đã đọc")]
         public async Task<ActionResult> MarkAsRead(int id)
         {
             var userId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
@@ -56,11 +61,25 @@ namespace SRPM.API.Controllers
 
             return NoContent();
         }
+
         [HttpPatch("mark-all-as-read")]
+        [SwaggerOperation(Summary = "Đánh dấu tất cả thông báo là đã đọc")]
         public async Task<ActionResult> MarkAllAsRead()
         {
             var userId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
             var success = await _notificationService.MarkAllAsReadAsync(userId);
+            if (!success)
+                return BadRequest();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        [SwaggerOperation(Summary = "Xóa thông báo")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var userId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+            var success = await _notificationService.DeleteAsync(id, userId);
             if (!success)
                 return BadRequest();
 
